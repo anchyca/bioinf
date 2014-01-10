37453329a1990b9e3a4add473d2c575dff611918
Grim <filip.grilec@fer.hr>
2014-01-10 20:38:46 +0100

    'grills'

--- c/bioinf.c
+++ c/bioinf.c
@@ -3,85 +3,105 @@
 #include <time.h>
 
 
-int binary_replace(int *lis, int left, int right, int key) {
-        int mid;
-        //Standard binary search
-        for (mid = (left+right)/2; left <= right; mid = (left+right)/2) {
-                if (lis[mid] > key) {
+int binary_rep(int *arr, int left, int right, int key) {
+        //Vrši se standardna binarna pretraga i ubacuje se broj u rezultatni niz.
+
+        int mid=(left+right)/2;
+        for (mid; left <= right; mid = (left+right)/2)
+        {
+                if (arr[mid] > key)
                         right = mid - 1;
-                } else if (lis[mid] == key) {
+                else if (arr[mid] == key)
                         return mid;
-                } else if (mid+1 <= right && lis[mid+1] >= key) {
-                        lis[mid+1] = key;
+                else if (mid+1 <= right && arr[mid+1] >= key)
+                    {
+                        arr[mid+1] = key;
                         return mid+1;
-                } else {
+                    }
+                else
                         left = mid + 1;
-                }
         }
-        if (mid == left) {
-                lis[mid] = key;
-                return mid;
+        if (mid == left)
+        {
+            arr[mid] = key;
+            return mid;
         }
-        lis[mid+1] = key;
+        arr[mid+1] = key;
         return mid+1;
 }
 
 int main(void) {
-        //Variables for time measurement
+        //Varijable za mjerenje vremena
         clock_t begin, end;
         begin = clock();
         double time_spent;
 
-        int i=0, tmp,lis_length = -1,storage=0;
-        int *answer;
-        int *A = (int*) malloc(1*sizeof(int));
+        int i=0, tmp,length = -1,storage=0;
+        //Alokacija memorije za prvi clan niza
+        int *arr_0 =(int*) malloc(1*sizeof(int));
 
-        //Reading the input file
+        //Citanje iz ulazne datoteke
         FILE *fp = fopen("u.txt","r");
+        if(fp==NULL)
+        {
+            printf("Greška!");
+            return 0;
+        }
         while(fscanf(fp,"%d, ",&tmp)>0)
          {
-             A[i]=tmp;
+             arr_0[i]=tmp;
              i++;
-             //Reallocate memory because of unknown list size
-             A=(int*)realloc(A,(i+1)*sizeof(int));
+             //Realokacija za dodatak svakog novog clana
+             arr_0=(int*)realloc(arr_0,(i+1)*sizeof(int));
          }
         fclose(fp);
 
+        // i iz for petlje sluzio za zapis duljine ulaznog niza
+        // te alokaciju memorije sukladno toj velicini
+        int size=i;
+        int *arr_n = (int*) malloc(i*sizeof(int));    //lista brojeva
+        int *index = (int*) malloc(i*sizeof(int));  //lista indeksa
 
-        int size=i; //Last index shows the size of the list
-        int *LIS = (int*) malloc(i*sizeof(int));
-        int *index = (int*) malloc(i*sizeof(int));
 
-        //Manual memory allocation calclulation
-        storage =5*sizeof(int)+4*sizeof(int*)+size*3*sizeof(int);
         for (i = 0; i < size; i++)
         {
-                index[i] = binary_replace(LIS, 0, i, A[i]);
-                if (lis_length < index[i])
+                //vrsi binarnu pretragu u danom intervalu te vrši zamjenu
+                index[i] = binary_rep(arr_n, 0, i, arr_0[i]);
+                if (length < index[i])
                 {
-                    lis_length = index[i];
+                    length = index[i];
                 }
         }
-
-        answer = (int*) malloc((lis_length+1) * sizeof(int));
-        for (i = size-1, tmp = lis_length; i >= 0; i--)
+        //dodjelji
+        int *res = (int*) malloc((length+1) * sizeof(int));
+        for (i = size-1, tmp = length; i >= 0; i--)
         {
                 if (index[i] == tmp)
                 {
-                     answer[tmp] = A[i];
+                     res[tmp] = arr_0[i];
                      tmp--;
                 }
         }
+        //Zapis u izlaznu datoteku
+        //Za promjenu ulaza+izlaza iz hardkodirane verzije treba samo koristiti cmd argumente :)
 
-        //Open and write the list into the output file
         fp = fopen("output.txt","w");
-        fprintf(fp,"[");
-        for (i = 0; i < lis_length; ++i) {
-                fprintf(fp,"%d, ", answer[i]);
+        //if it fails
+        if(fp==NULL)
+        {
+            printf("Greška!");
+            return 0;
         }
-        fprintf(fp,"%d]",answer[lis_length]);
+        //format izlaza
+        fprintf(fp,"[");
+        for (i = 0; i < length; ++i) {
+                fprintf(fp,"%d, ", res[i]);
+        }
+        fprintf(fp,"%d]",res[length]);
         fclose(fp);
-
+        //Racunanje potrebne memorije
+        //not sure if better way
+        storage =5*sizeof(int)+4*sizeof(int*)+size*3*sizeof(int);
         //Calculate required time
         end = clock();
         time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
